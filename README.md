d0799b7757e3c8bb8e818ff188551c8c4fe8b7e4
busitaro <busitaro10@gmail.com>
2023-08-29 09:11:15 +0900

    :tada: first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # upload-to-onedrive
-onedriveにアップロードする為のスクリプト
+
+onedrive にアップロードする為のスクリプト
+
+# Author
+
+作成情報を列挙する
+
+- busitaro
+- busitaro10@gmail.com
+
+# License
+
+ライセンスを明示する
+
+"Graph-Auth" is under [MIT license](https://en.wikipedia.org/wiki/MIT_License).
---
+++ upload_to_onedrive/upload.py
@@ -0,0 +1,38 @@
+import requests
+
+from graph_auth import read_token
+
+
+def upload(user_id: str, upload_file_path: str, upload_dest_file_path: str):
+    # リクエスト先設定
+    header = {"Authorization": f"Bearer {read_token()}"}
+    url = f"https://graph.microsoft.com/v1.0/users/{user_id}/drive/items/root:/{upload_dest_file_path}:/content"
+
+    # APIの実行
+    try:
+        response = requests.put(url, data=read_file(upload_file_path), headers=header)
+    except Exception:
+        print("An error occurred during requesting for api")
+
+    print(response.__dict__)
+
+    # レスポンスのチェック
+    if response.status_code != 201:
+        print("reponse status is not correct")
+        print(response._content.decode(encoding="utf-8"))
+
+    return response
+
+
+def read_file(file_path: str) -> bytes:
+    """
+    ファイルの内容をバイナリで読込む
+
+    Returns
+    -------
+    0: bytes
+        ファイル内容
+    """
+    with open(file_path, "rb") as file:
+        file_content = file.read()
+    return file_content
